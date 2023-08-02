63e958bae982e19f469f25efac22b98a121d06c9
Goodeny <32174827+goodeny@users.noreply.github.com>
2023-08-02 20:02:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -17,14 +17,17 @@ class App:
         self.entries = []
         self.numbers = []
         self.solution = []
+        self.value_slider = DoubleVar()
         self.install_lib()
         self.create_logo()
+        self.create_slider()
         self.generate_buttons()
+        self.create_button_restart()
         self.windows_blocks()
         self.create_button_solve()
     
     def windows_blocks(self):
-        self.w_blocks = Frame(self.window, height=550, width=613, bg='#373941')
+        self.w_blocks = Frame(self.window, height=550, width=600, bg='#373941')
         self.w_blocks.place(x=0, y=100)
         self.generate_matriz_block()
 
@@ -55,16 +58,28 @@ class App:
         self.logo_bg = PhotoImage(file='assets/logo.png', )
         self.logo_label = Label(self.window, image=self.logo_bg, bg='#373941')
         self.logo_label.place(x=30,y=30)
-            
+
+    def create_slider(self):
+        self.label_slider = Label(self.window, text='On bad computers lower values ​​may give wrong results \n\n| Hyper Boost: 0 | Medium: 1 | Recommended: 2 |', fg='white', bg='#373941')
+        self.label_slider.place(x=617, y=400)
+
+        self.slider = Scale(self.window, from_=0, to=100, resolution=1.0 ,orient='horizontal', variable=self.value_slider, command=self.update, bg='#373941', bd=0, activebackground="#373941", fg='white', highlightbackground='#373941', length=260)  
+        self.slider.place(x=630, y=450)
+        
     def create_button_solve(self):
         self.button_solve_img = PhotoImage(file='assets/button_solve.png')
-        self.button_solve = Button(self.window, image=self.button_solve_img, bg='#373941', activebackground='#373941', bd=0, command=lambda: threading.Thread(target=self.generate_sudoku).start())
-        self.button_solve.place(x=620, y=580)
+        self.button_solve = Button(self.window, image=self.button_solve_img, bg='#373941', activebackground='#373941', bd=0, cursor='hand2', command=lambda: threading.Thread(target=self.generate_sudoku).start())
+        self.button_solve.place(x=630, y=500)
 
     def create_button_solving(self):
         self.button_solving_img = PhotoImage(file='assets/button_solving.png')
-        self.button_solving = Button(self.window, image=self.button_solving_img, bg='#373941', activebackground='#373941', bd=0)
-        self.button_solving.place(x=620, y=580)
+        self.button_solving = Button(self.window, image=self.button_solving_img, bg='#373941', cursor='hand2', activebackground='#373941', bd=0)
+        self.button_solving.place(x=630, y=500)
+
+    def create_button_restart(self):
+        self.button_restart_img = PhotoImage(file='assets/button_restart.png')
+        self.button_restart = Button(self.window, image=self.button_restart_img, bd=0, bg='#373941', cursor='hand2', command=lambda: threading.Thread(target=self.restart).start())
+        self.button_restart.place(x=630, y=580)
 
     def generate_sudoku(self):
         self.button_solve.destroy()
@@ -101,7 +116,7 @@ class App:
     
     def generate_buttons(self):
         x1=620
-        y1=130
+        y1=100
 
         imgs = ['assets/buttons/1.png',
                 'assets/buttons/2.png',
@@ -124,63 +139,83 @@ class App:
         self.button_pad_img8 = PhotoImage(file=imgs[7])
         self.button_pad_img9 = PhotoImage(file=imgs[8])
 
-        self.button_pad = Button(self.window,image=self.button_pad_img1, text=1, bd= 0, command=lambda: self.input_number(1))
+        self.button_pad = Button(self.window,image=self.button_pad_img1, text=1, bd= 0, bg="#373941", activebackground="#373941", cursor='hand2',command=lambda: self.input_number(1))
         self.button_pad.place(x=x1,y=y1)
 
-        self.button_pad = Button(self.window,image=self.button_pad_img2, bd= 0, command=lambda: self.input_number(2))
+        self.button_pad = Button(self.window,image=self.button_pad_img2, bd= 0, bg="#373941", activebackground="#373941", cursor='hand2',command=lambda: self.input_number(2))
         self.button_pad.place(x=x1+100,y=y1)
 
-        self.button_pad = Button(self.window,image=self.button_pad_img3, bd= 0, command=lambda: self.input_number(3))
+        self.button_pad = Button(self.window,image=self.button_pad_img3, bd= 0, bg="#373941", activebackground="#373941", cursor='hand2',command=lambda: self.input_number(3))
         self.button_pad.place(x=x1+200,y=y1)
 
-        self.button_pad = Button(self.window,image=self.button_pad_img4, bd= 0, command=lambda: self.input_number(4))
+        self.button_pad = Button(self.window,image=self.button_pad_img4, bd= 0, bg="#373941", activebackground="#373941", cursor='hand2',command=lambda: self.input_number(4))
         self.button_pad.place(x=x1,y=y1+100)
 
-        self.button_pad = Button(self.window,image=self.button_pad_img5, bd= 0, command=lambda: self.input_number(5))
+        self.button_pad = Button(self.window,image=self.button_pad_img5, bd= 0, bg="#373941", activebackground="#373941", cursor='hand2',command=lambda: self.input_number(5))
         self.button_pad.place(x=x1+100,y=y1+100)
 
-        self.button_pad = Button(self.window,image=self.button_pad_img6, bd= 0, command=lambda: self.input_number(6))
+        self.button_pad = Button(self.window,image=self.button_pad_img6, bd= 0, bg="#373941", activebackground="#373941", cursor='hand2',command=lambda: self.input_number(6))
         self.button_pad.place(x=x1+200,y=y1+100)
 
-        self.button_pad = Button(self.window,image=self.button_pad_img7, bd= 0, command=lambda: self.input_number(7))
+        self.button_pad = Button(self.window,image=self.button_pad_img7, bd= 0, bg="#373941", activebackground="#373941", cursor='hand2',command=lambda: self.input_number(7))
         self.button_pad.place(x=x1,y=y1+200)
 
-        self.button_pad = Button(self.window,image=self.button_pad_img8, bd= 0, command=lambda: self.input_number(8))
+        self.button_pad = Button(self.window,image=self.button_pad_img8, bd= 0, bg="#373941", activebackground="#373941", cursor='hand2',command=lambda: self.input_number(8))
         self.button_pad.place(x=x1+100,y=y1+200)
 
-        self.button_pad = Button(self.window,image=self.button_pad_img9, bd= 0, command=lambda: self.input_number(9))
+        self.button_pad = Button(self.window,image=self.button_pad_img9, bd= 0, bg="#373941", activebackground="#373941", cursor='hand2',command=lambda: self.input_number(9))
         self.button_pad.place(x=x1+200,y=y1+200)
 
     def input_number(self, n):
         keyboard.press(f'{n}')
 
+    def update(self, event=None):
+        current_value = self.value_slider.get() / 10.0
+        #print(f'value: {current_value}')
+
     def solve_sudoku(self):
         puzzle = Sudoku(3, 3, board=board)
         solution = puzzle.solve()
-        print(solution.board)
+        #print(solution.board)
         for i in solution.board:
             for y in i:
                 self.solution.append(y)
         #print(self.solution)
         self.fill_website()
-            
-    
+
+    def fill_software(self):
+        for i, entry in enumerate(self.entries):
+                entry.delete(0,END)
+                entry.insert(0, self.solution[i])
+        time.sleep(2)
+
+    def restart(self):
+        self.solution.clear()
+        self.numbers.clear()
+        for i, entry in enumerate(self.entries):
+                entry.delete(0,END)
+        
     def fill_website(self):
-        time.sleep(3)
+        time.sleep(5)
         count = 1
         for i in range(0,81):
             if keyboard.is_pressed('0'):
                 break
             else:
-                time.sleep(0.3)
-                keyboard.press(f'{self.solution[i]}')
-                keyboard.press('right')
-                if count == 9:
-                    keyboard.press('down')
-                    for i in range(9):
-                        keyboard.press('left')
-                    count = 0
-                count += 1
+                try:
+                    time.sleep(self.value_slider.get() / 10.0)
+                    keyboard.press(f'{self.solution[i]}')
+                    keyboard.press('right')
+                    if count == 9:
+                        keyboard.press('down')
+                        for i in range(9):
+                            keyboard.press('left')
+                        count = 0
+                    count += 1
+                except:
+                    self.button_solving.destroy()
+                    self.create_button_solve()
+        self.fill_software()
         self.button_solving.destroy()
         self.create_button_solve()
 
@@ -190,8 +225,7 @@ class App:
             os.system('pip install py-sudoku')
             os.system('pip install keyboard')
         except:
-            pass
-
+            os.system('Try install "pip install py-sudoku"\n"pip install keyboard"\nDirect download: https://github.com/goodeny/Sudoku-Engine')
 
 if __name__ == "__main__":
     app = App()
--- sudoku_buttons.py
+++ sudoku_buttons.py
@@ -10,9 +10,9 @@ class Buttons:
     def generate(self,x1 ,y1,img):
         self.load_block_img = PhotoImage(file='assets/block.png')
 
-        self.block = Label(self.window, image=img, width=48,height=48, bg='#373941')
+        self.block = Label(self.window, image=img, width=48,height=48, bg='#373941', cursor='hand2')
         self.block.place(x=x1,y=y1)
 
-        self.entry = Entry(self.block, width=2, font='Arial 30',justify="center", bg='white', fg='black', bd=0)
+        self.entry = Entry(self.block, width=2, font='Arial 30',justify="center", bg='white', fg='black', bd=0, cursor='hand2')
         self.entry.place(x=0, y=0)
         
